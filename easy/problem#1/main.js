a826db2ac7e156916007d721549f53286198bda2
Fojan Babaali <fozhanmail@gmail.com>
2021-08-11 23:13:31 -0700

    Update main.js

--- easy/problem#1/main.js
+++ easy/problem#1/main.js
@@ -35,11 +35,14 @@ let holder = [];
 function twoSum(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     let secondNum = target - nums[i];
-    if (nums.includes(secondNum)) {
+    if (nums.includes(secondNum) && secondNum !== nums[i]) {
       holder.push(i, nums.indexOf(secondNum));
+      return holder;
+    } else if (nums.includes(secondNum)) {
+      holder.push(i, nums.indexOf(secondNum)+1);
       return holder;
     }
   }
 }
-const x = twoSum([1, 3, 5, 4], 7);
+const x = twoSum([2,0,11,15,7], 9);
 console.log(x);
