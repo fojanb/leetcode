d51e26e9fded2660e717b493549e64097314ddf0
Fojan Babaali <fozhanmail@gmail.com>
2021-08-13 23:28:30 -0700

    Update main.js

--- easy/problem#3/main.js
+++ easy/problem#3/main.js
@@ -2,11 +2,9 @@ const rev = function (number) {
   let str = number.toString();
   let arr = str.split("");
   if (number < 0) {
-    return parseInt(arr.reverse().join(""))*(-1);
-  }
-  else{
+    return parseInt(arr.reverse().join("")) * -1;
+  } else {
     return parseInt(arr.reverse().join(""));
-
   }
 };
 console.log(rev(1020));
