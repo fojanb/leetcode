82a9a54768a26fc5189ab2320f606a803ec774e5
Fojan Babaali <fozhanmail@gmail.com>
2021-08-14 22:24:03 -0700

    Update main.js

--- easy/problem#1/main.js
+++ easy/problem#1/main.js
@@ -1,15 +1,23 @@
 let holder = [];
 function twoSum(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    let secondNum = target - nums[i];
-    if (nums.includes(secondNum) && secondNum !== nums[i]) {
-      holder.push(i, nums.indexOf(secondNum));
-      return holder;
-    } else if (nums.includes(secondNum)) {
-      holder.push(i, nums.indexOf(secondNum)+1);
-      return holder;
+  if (-109 <= target && target <= 109) {
+    for (let i = 0; i < nums.length; i++) {
+      let secondNum = target - nums[i];
+      if (nums.includes(secondNum) && secondNum !== nums[i]) {
+        holder.push(i, nums.indexOf(secondNum));
+        return holder;
+      } else if (nums.includes(secondNum)) {
+        holder.push(i, nums.indexOf(secondNum) + 1);
+        return holder;
+      } else {
+        if (!nums.includes(secondNum)) {
+          return "Target is not found";
+        }
+      }
     }
+  } else {
+    alert("Please set a target between -109 and 109");
   }
 }
-const x = twoSum([2,0,11,15,7], 9);
+const x = twoSum([9, -10, -109], 100);
 console.log(x);
