280e368e93f1268f422bf209f26a35f689eddb11
Fojan Babaali <fozhanmail@gmail.com>
2021-09-20 16:47:03 -0700

    Update main.js

--- easy/problem#4/main.js
+++ easy/problem#4/main.js
@@ -1,13 +1,34 @@
 const data = {
   form: document.querySelector("#form"),
-  romanNumber : "",
+  romanNumber: "",
+  integerNumber: document.querySelector(".IntegerNumber"),
+  romanLetters: {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  },
 };
-
-const romanToInt = (e) => {
+function romanToInt(e) {
   e.preventDefault();
-  romanNumber = e.target.RomanNumber.value;
-  console.log(romanNumber)
-
-};
-
+  data.romanNumber = e.target.RomanNumber.value;
+  data.integerNumber.dispatchEvent(new CustomEvent("convert"));
+}
+function displayInteger() {
+  let romanToArray = data.romanNumber.split("");
+  // i.e. ['V', 'I', 'M', 'L', 'I', 'X']
+  romanToArray.map((letter) => {
+    switch (letter) {
+        case 'I':
+            
+            case 'X':
+                case 'C'
+    }
+  });
+  data.integerNumber.innerHTML = `<div class="rnumber">${romanToArray}</div>`;
+}
+data.integerNumber.addEventListener("convert", displayInteger);
 data.form.addEventListener("submit", romanToInt);
