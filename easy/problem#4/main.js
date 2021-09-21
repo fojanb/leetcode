b1a49d705f1ade5ba201d14742591322b7d63645
Fojan Babaali <fozhanmail@gmail.com>
2021-09-20 17:28:08 -0700

    Update main.js

--- easy/problem#4/main.js
+++ easy/problem#4/main.js
@@ -12,23 +12,74 @@ const data = {
     M: 1000,
   },
 };
-function romanToInt(e) {
+function handleSubmit(e) {
   e.preventDefault();
   data.romanNumber = e.target.RomanNumber.value;
   data.integerNumber.dispatchEvent(new CustomEvent("convert"));
 }
 function displayInteger() {
   let romanToArray = data.romanNumber.split("");
+  let total = 0;
   // i.e. ['V', 'I', 'M', 'L', 'I', 'X']
-  romanToArray.map((letter) => {
+  romanToArray.map((letter, index) => {
+    if (letter === "I") {
+      //   Check for next letter if it is "V" or "X"
+      switch (romanToArray[index + 1]) {
+        case "V":
+          total += data.romanLetters.V - data.romanLetters.I;
+          break;
+        case "X":
+          total += data.romanLetters.X - data.romanLetters.I;
+          break;
+        default:
+          total += data.romanLetters.I;
+      }
+    }
+    if (letter === "X") {
+      //   Check for next letter if it is "L" or "C"
+      switch (romanToArray[index + 1]) {
+        case "L":
+          total += data.romanLetters.L - data.romanLetters.X;
+          break;
+        case "C":
+          total += data.romanLetters.C - data.romanLetters.X;
+          break;
+        default:
+          total += data.romanLetters.X;
+      }
+    }
+    if (letter === "C") {
+      //   Check for next letter if it is "D" or "M"
+      switch (romanToArray[index + 1]) {
+        case "D":
+          total += data.romanLetters.D - data.romanLetters.X;
+          break;
+        case "M":
+          total += data.romanLetters.M - data.romanLetters.X;
+          break;
+        default:
+          total += data.romanLetters.C;
+      }
+    }
+    // ------------
     switch (letter) {
-        case 'I':
-            
-            case 'X':
-                case 'C'
+      case "V":
+        total += data.romanLetters.V;
+        break;
+      case "L":
+        total += data.romanLetters.L;
+        break;
+      case "D":
+        total += data.romanLetters.D;
+        break;
+      case "M":
+        total += data.romanLetters.M;
+        break;
+      default:
+        return total;
     }
   });
-  data.integerNumber.innerHTML = `<div class="rnumber">${romanToArray}</div>`;
+  data.integerNumber.innerHTML = `<div class="rnumber">${total}</div>`;
 }
 data.integerNumber.addEventListener("convert", displayInteger);
-data.form.addEventListener("submit", romanToInt);
+data.form.addEventListener("submit", handleSubmit);
